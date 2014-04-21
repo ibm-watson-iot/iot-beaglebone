b7ab573dd74fe0531320d1aa2d6c1f27693e9fc2
Stuart Hayton <hayton@trouble.hayton.me.uk>
2014-04-21 23:57:50 +0100

    fill out README.md and add init.d script

--- samples/nodered/setup.sh
+++ samples/nodered/setup.sh
@@ -5,8 +5,14 @@ REDVERSION=0.7.1
 apt-get install unzip 
 wget https://github.com/node-red/node-red/archive/$REDVERSION.zip
 unzip $REDVERSION.zip
+
+cp node-red /etc/init.d
+sed -i "s/RED_DIR/$MYDIR/g" /etc/init.d/node-red
+
+cp flows_beaglebone.json node-red-$REDVERSION
+
 cd node-red-$REDVERSION
-npm install sensortag setmac
+npm install sensortag getmac
 npm install --production
 
 # add contributed nodes including sensortag
@@ -15,10 +21,7 @@ npm install --production
 # add getmac to global context
 sed -i "s/functionGlobalContext: { }/functionGlobalContext: { getmac:require('getmac') }/g" settings.js
 
-# add the standard flow and code file for reference
-git clone flow / function 
-
-# install runner
+# setup runner
 update.rc /etc/inid.d/node-red defaults 
 service node-red start 
 
