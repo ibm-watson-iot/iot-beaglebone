4184a57c03e474f247cd27ec8b0e510c106cf8a2
alsm <asm@rndm.io>
2014-04-25 13:50:33 +0100

    Check for libbluetooth-dev being installed

--- samples/nodejs/setup.sh
+++ samples/nodejs/setup.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-
+dpkg --status libbluetooth-dev > /dev/null 2>&1
+if [ "$?" = "1" ]; then
+	echo "Please install bluetooth development libraries, run:\n\tsudo apt-get install libbluetooth-dev"
+	exit 1
+fi
+	
 # install pre-requisites
 npm install async getmac mqtt sensortag
 
